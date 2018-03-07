e75ce6e51fe6a7299219da25aa16b8cbf62b0fbd
Paweł Adamiec <pawel.adamiec@cgi.com>
2018-03-07 15:06:38 +0100

    dodanie obsługi CORS i wstępna konfiguracja spring security. Działające zapisywanie plików

--- src/main/java/com/pawel/filetrans/controllers/FileController.java
+++ src/main/java/com/pawel/filetrans/controllers/FileController.java
@@ -2,57 +2,55 @@ package com.pawel.filetrans.controllers;
 
 import com.pawel.filetrans.dto.UploadStatusDTO;
 import com.pawel.filetrans.service.FileService;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 @RequestMapping(value = {"/file"})
 public class FileController {
 
-    private final FileService fileService;
+  private final FileService fileService;
 
-    @Autowired
-    public FileController(FileService fileService) {
-        this.fileService = fileService;
+  @Autowired
+  public FileController(FileService fileService) {
+    this.fileService = fileService;
+  }
+
+  @PostMapping("/upload")
+  public ResponseEntity<String> handleFileUpload(HttpServletRequest request, @RequestParam("file") MultipartFile multipartFile) {
+    String message;
+
+    if (!multipartFile.isEmpty()) {
+      String filename = multipartFile.getOriginalFilename();
+      try {
+        fileService.saveFile(request, multipartFile);
+        message = "File uploaded: " + filename;
+        log.info(message);
+        return ResponseEntity.status(HttpStatus.OK).body(message);
+      } catch (IOException e) {
+        message = "Unable to upload file: " + filename;
+        log.error(message);
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(message);
+      }
     }
-
-    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = {"multipart/form-data"}, produces = {"application/json"})
-    @ResponseStatus(HttpStatus.OK)
-    public UploadStatusDTO handleFileUpload(HttpServletRequest request, @RequestParam("file") MultipartFile multipartFile) {
-
-        if (!multipartFile.isEmpty()) {
-            String filename = multipartFile.getOriginalFilename();
-            try {
-                fileService.saveFile(request, multipartFile);
-                return getStatusDTO(filename, "OK", "");
-
-            } catch (IOException e) {
-                log.error("Unable to upload file: {}", filename);
-                e.printStackTrace();
-                return getStatusDTO(filename, "ERROR", "Unable to upload file!");
-            }
-        }
-        return getStatusDTO("", "ERROR", "Empty file!");
-    }
-
-    @GetMapping("/greeting")
-    public UploadStatusDTO greeting(@RequestParam(required=false, defaultValue="World") String name) {
-        System.out.println("==== in greeting ====");
-        return new UploadStatusDTO("1", name);
-    }
-
-    private UploadStatusDTO getStatusDTO(String fileName, String status, String description) {
-        UploadStatusDTO statusDTO = new UploadStatusDTO();
-        statusDTO.setFileName(fileName);
-        statusDTO.setStatus(status);
-        statusDTO.setErrorDescription(description);
-        return statusDTO;
-    }
+    message = "Empty file!";
+    log.error(message);
+    return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(message);
+  }
 }
--- src/main/resources/public/hello.js
+++
@@ -1,7 +0,0 @@
-angular.module('demo', [])
-    .controller('Hello', function($scope, $http) {
-        $http.get('http://localhost:8090/file/greeting?name=Wacek').
-        then(function(response) {
-            $scope.greeting = response.data;
-        });
-    });
